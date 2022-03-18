7ab5d1d6ae65d1c20e695a2d71a010e5d146c93a
Alaa <davelion3@gmail.com>
2022-03-18 09:59:37 +0300

    Implement the freecodecamp project specifications

--- server.js
+++ server.js
@@ -25,7 +25,13 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.socket.remoteAddress,
+    language: req.headers["accept-language"],
+    software: req.get('User-Agent')
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
